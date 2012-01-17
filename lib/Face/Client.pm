122399f6bd4057e4ba0e351b16189225c6388b18
Arnaud "Arhuman" Assad <arhuman@gmail.com>
2012-01-17 05:10:37 +0100

    Add basic pod doc for methods

--- lib/Face/Client.pm
+++ lib/Face/Client.pm
@@ -83,6 +83,12 @@ sub new {
 
 =head2 faces_detect
 
+Returns tags for detected faces in one or more photos, with geometric information of the tag, eyes, nose and mouth, as well as various attributes such as gender, is wearing glasses, and is smiling.
+
+Photos can also be uploaded directly in the API request. A requests that uploads a photo must be formed as a MIME multi-part message sent using POST data. Each argument, including the raw image data, should be specified as a separate chunk of form data.
+
+More information : http://developers.face.com/docs/api/faces-detect/
+
 =cut
 
 sub faces_detect {
@@ -100,6 +106,12 @@ sub faces_detect {
 }
 
 =head2 faces_train
+
+Calls the training procedure for the specified UIDs, and reports back changes.
+
+The training procedure uses information from previous tags.save calls to build a training set for the specified UIDs. For Facebook UIDs, you can skip the tags.save stage and call faces.train directly - we will use the passed credentials to create a training set from the users' tagged photos on Facebook.
+
+More information : http://developers.face.com/docs/api/faces-train/
 
 =cut
 
@@ -119,6 +131,18 @@ sub faces_train {
 
 =head2 faces_recognize
 
+Attempts to detect and recognize one or more user IDs' faces, in one or more photos. For each detected face, the face.com engine will return the most likely user IDs, or empty result for unrecognized faces. In addition, each tag includes a threshold score - any score below this number is considered a low-probability hit.
+
+You can also save the recognized faces by calling tags.save with the returned temporary tag id (tid), along with the relevant user ID. Saving tags is also the way to train the platform how users look like, for later calls to faces.recognize.
+
+The first step in recognition is face detection, which is applied automatically for each photo sent for recognition. Therefor these calls generally use the same tag output with the addition of recognized user IDs (see faces.detect for more details and usage notes).
+
+In addition, when passing specific uids, (not special list as "friends" and "all"), we will return a list of uids that have no train set, and there for cannot be recognized, under "no_training_set" list.
+
+Photos can also be uploaded directly in the API request. A requests that uploads a photo must be formed as a MIME multi-part message sent using POST data. Each argument, including the raw image data, should be specified as a separate chunk of form data.
+
+More information : http://developers.face.com/docs/api/faces-recognize/
+
 =cut
 
 sub faces_recognize {
@@ -136,6 +160,12 @@ sub faces_recognize {
 }
 
 =head2 faces_status
+
+Reports training set status for the specified UIDs.
+
+This method only reports the status of the current training-set status, and does not change it. To improve, or create training set for a uid, use faces.train.
+
+More information : http://developers.face.com/docs/api/faces-status/
 
 =cut
 
@@ -155,6 +185,10 @@ sub faces_status {
 
 =head2 tags_add
 
+Add a (manual) face tag to a photo. Use this method to add face tags where those were not detected for completeness of your service. Manual tags are treated like automatic tags, except they are not used to train the system how a user looks like. See the tags.save method to learn how to save automatic face tags for recognition purposes.
+
+More information : http://developers.face.com/docs/api/tags-add/
+
 =cut
 
 sub tags_add {
@@ -172,6 +206,10 @@ sub tags_add {
 }
 
 =head2 tags_remove
+
+Remove a previously saved face tag from a photo.
+
+More information : http://developers.face.com/docs/api/tags-remove/
 
 =cut
 
@@ -191,6 +229,12 @@ sub tags_remove {
 
 =head2 tags_get
 
+Returns saved tags in one or more photos, or for the specified User ID(s). This method also accepts multiple filters for finding tags corresponding to a more specific criteria such as front-facing, recent, or where two or more users appear together in same photos.
+
+Photos can also be uploaded directly in the API request. A requests that uploads a photo must be formed as a MIME multi-part message sent using POST data. Each argument, including the raw image data, should be specified as a separate chunk of form data.
+
+More information : http://developers.face.com/docs/api/tags-get/
+
 =cut
 
 sub tags_get {
@@ -208,6 +252,12 @@ sub tags_get {
 }
 
 =head2 tags_save
+
+Saves a face tag. Use this method to save tags for training the face.com index, or for future use of the faces.detect and tags.get methods.
+
+This method accepts 2 primary parameters: User ID (uid) and a list of one or more Tag IDs (tids). The uid field represents a single user ID to associate with the saved tags with, while the tids is a list of tag ids previously acquired through calls to faces.detect or faces.recognize. When photos are processed through the detect and recognize methods, their response includes temporary tag IDs for use in subsequent tags.save calls. The temporary tag IDs are replaced with permanent tag IDs after calling the tags.save method, and are returned in the method's response for future reference.
+
+More information : http://developers.face.com/docs/api/tags-save/
 
 =cut
 
@@ -227,6 +277,10 @@ sub tags_save {
 
 =head2 account_limits
 
+Returns current rate limits for the account represented by the passed API key and Secret.
+
+More information : http://developers.face.com/docs/api/account-limits/
+
 =cut
 
 sub account_limits {
@@ -244,6 +298,10 @@ sub account_limits {
 }
 
 =head2 account_users
+
+Returns current users registered in the account's private namespaces. Users in a private namespace get registered implicitly through tags.save calls.
+
+More information : http://developers.face.com/docs/api/account-users/
 
 =cut
 
@@ -336,27 +394,25 @@ You can also look for information at:
 
 =over 4
 
-=item * RT: CPAN's request tracker (report bugs here)
+=item * Github (report bugs here)
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Face-Client>
+L<https://github.com/arhuman/Face-Client>
 
 =item * AnnoCPAN: Annotated CPAN documentation
 
 L<http://annocpan.org/dist/Face-Client>
 
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Face-Client>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Face-Client/>
-
 =back
+
+More information about Face.com service :
+
+L<http://developers.face.com/docs/api>
 
 
 =head1 ACKNOWLEDGEMENTS
 
+Thanks to Face.com for the service they provide.
+Thanks to Jaguar Network for allowing me to publish my work.
 
 =head1 LICENSE AND COPYRIGHT
 
